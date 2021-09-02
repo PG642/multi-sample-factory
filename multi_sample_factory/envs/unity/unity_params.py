f02e8b07579b07d3d7bef1653754b23aa3339ae3
Konstantin Ramthun <konstantin_ramthun@web.de>
2021-09-02 13:25:53 +0200

    Added unity default parameters, based on previous experiments with ml-agents

--- multi_sample_factory/envs/unity/unity_params.py
+++ multi_sample_factory/envs/unity/unity_params.py
@@ -3,12 +3,14 @@ def unity_override_defaults(env, parser):
     parser.set_defaults(
         encoder_type='mlp',
         encoder_subtype='mlp_mujoco',
-        hidden_size=256,
-        batch_size=512,
+        hidden_size=512, # based on experiments with ml-agents
+        batch_size=2048, # based on experiments with ml-agents
         with_vtrace=False,
         use_rnn=False,
         recurrence=1,
-        nonlinearity='relu'
+        nonlinearity='relu',
+        learning_rate=0.0003, # based on experiments with ml-agents
+        gae_lambda=0.99 # based on experiments with ml-agents
     )
 
 # noinspection PyUnusedLocal
