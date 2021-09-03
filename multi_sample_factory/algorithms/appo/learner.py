12a220cf4387a15aa751e407f722423e2caf29d5
Konstantin Ramthun <konstantin.ramthun@tu-dortmund.de>
2021-09-03 10:23:24 +0200

    Added logging information for observations.

--- multi_sample_factory/algorithms/appo/learner.py
+++ multi_sample_factory/algorithms/appo/learner.py
@@ -713,7 +713,7 @@ class LearnerWorker:
                         with timing.add_time('bptt_forward_core'):
                             core_output_seq, _ = self.actor_critic.module.forward_core(head_output_seq, rnn_states)
                         core_outputs = build_core_out_from_seq(core_output_seq, inverted_select_inds)
-                
+                log.debug(mb.obs)
                 head_outputs, core_outputs, result = self.actor_critic(mb.obs, rnn_states, with_action_distribution=True)
 
                 num_trajectories = head_outputs.size(0) // recurrence
