fa12ff3d5bb3970e69a4ff07657c4f87e413587d
Konstantin Ramthun <konstantin.ramthun@tu-dortmund.de>
2021-09-17 17:52:27 +0200

    Fixed log_prob in TanhNormal

--- multi_sample_factory/algorithms/utils/distributions.py
+++ multi_sample_factory/algorithms/utils/distributions.py
@@ -12,22 +12,18 @@ class TanhNormal(torch.distributions.Normal, ABC):
         super().__init__(loc, scale)
         self.transform = torch.distributions.transforms.TanhTransform(cache_size=1)
 
-    def sample(self, sample_shape=torch.Size([])):
-        unsquashed_sample = super().sample(sample_shape)
+    def sample(self):
+        unsquashed_sample = super().sample()
         squashed = self.transform(unsquashed_sample)
         return squashed
 
-    def _inverse_tanh(self, value):
-        EPSILON = 1e-7  # Small value to avoid divide by zero
-
-        capped_value = torch.clamp(value, -1 + EPSILON, 1 - EPSILON)
-        return 0.5 * torch.log((1 + capped_value) / (1 - capped_value) + EPSILON)
-
     def log_prob(self, value):
-        unsquashed = self.transform.inv(value)
-        return self.log_prob_of_normal_with_epsilon(unsquashed) - self.transform.log_abs_det_jacobian(
-            unsquashed, value
-        )
+        EPSILON = 1e-5  # Small value to avoid divide by zero
+        capped_value = torch.clamp(value, -1 + EPSILON, 1 - EPSILON)
+        unsquashed = self.transform.inv(capped_value)
+        log_prob_of_normal_with_epsilon = self.log_prob_of_normal_with_epsilon(unsquashed)
+        log_abs_det_jacobian = self.transform.log_abs_det_jacobian(unsquashed, capped_value)
+        return log_prob_of_normal_with_epsilon - log_abs_det_jacobian
 
     def log_prob_of_normal_with_epsilon(self, value):
         """
@@ -37,7 +33,7 @@ class TanhNormal(torch.distributions.Normal, ABC):
         var = self.scale ** 2
         log_scale = torch.log(self.scale + EPSILON)
         return (
-            -((value - self.loc) ** 2) / (2 * var + EPSILON)
-            - log_scale
-            - math.log(math.sqrt(2 * math.pi))
+                -((value - self.loc) ** 2) / (2 * var + EPSILON)
+                - log_scale
+                - math.log(math.sqrt(2 * math.pi))
         )
