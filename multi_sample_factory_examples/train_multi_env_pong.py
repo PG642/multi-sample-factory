87a86bac20e53ea08c5c42007e7e093df09618d6
Horrible22232 <sorrybutiwin@web.de>
2021-11-23 18:02:13 +0100

    ++

--- multi_sample_factory_examples/train_multi_env_pong.py
+++ multi_sample_factory_examples/train_multi_env_pong.py
@@ -40,7 +40,7 @@ class MultiEnvPong(gym.Env):
 
     def step(self, actions):
         obs_n, reward_n, done_n, info =  self.env.step(actions)
-        return np.array(obs_n), np.array(reward_n), np.array(done_n), info
+        return [np.array(obs) for obs in obs_n], [np.array(reward) for reward in reward_n], [np.array(done) for done in done_n], info
 
     def render(self, mode='human'):
         self.env.render(mode)
