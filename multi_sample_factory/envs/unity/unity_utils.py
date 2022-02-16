aa079229970b421c35ed4bd509b0ef75a6f2c8c7
Konstantin Ramthun <konstantin_ramthun@web.de>
2022-02-16 13:02:47 +0100

    Fixed import of EnvironmentParametersChannel

--- multi_sample_factory/envs/unity/unity_utils.py
+++ multi_sample_factory/envs/unity/unity_utils.py
@@ -3,7 +3,8 @@ from typing import List
 
 from mlagents_envs.environment import UnityEnvironment
 from gym_unity.envs import UnityToGymWrapper
-from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel, EnvironmentParametersChannel
+from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+from mlagents_envs.side_channel.environment_parameters_channel import  EnvironmentParametersChannel
 from os.path import join
 import random
 
