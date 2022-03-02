6c577ad8ff845dc20daf2601a0a21685ac56e3b0
Horrible22232 <sorrybutiwin@web.de>
2022-03-02 21:19:25 +0100

    ++

--- multi_sample_factory/envs/unity/unity_utils.py
+++ multi_sample_factory/envs/unity/unity_utils.py
@@ -27,13 +27,14 @@ UNITY_ENVS = [
     UnitySpec('unity_saving_training_mixed', 'saving_training_mixed', []),
     UnitySpec('unity_rocket_league_saving_training_single_discrete', 'rocket_league_saving_training_single_discrete', []),
     UnitySpec('unity_striker', 'striker', []),
-    # You can add more unity environments here if needed
+    # You can add more single agent unity environments here if needed
 ]
 
 UNITY_MULTI_ENVS = [
     UnitySpec('unity_multi_agent_env', 'multi_agent_env', []),
     UnitySpec('unity_1v1', "1v1", []),
     UnitySpec("unity_1v1alternativeVerteiltesLernen", "1v1alternativeVerteiltesLernen", []),
+    # You can add more multi agent unity environments here if needed
 ]
 
 
